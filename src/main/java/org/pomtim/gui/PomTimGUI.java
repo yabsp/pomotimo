20367a5ec53c01dad2040a5f4fc96a41bafd4b2e
Yanick <yanick.spichty@gmail.com>
2025-02-06 16:07:43 +0100

    Add icon and TimerPane

--- src/main/java/org/pomtim/gui/PomTimGUI.java
+++ src/main/java/org/pomtim/gui/PomTimGUI.java
@@ -1,29 +1,22 @@
 package org.pomtim.gui;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+import javafx.scene.image.Image;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class PomTimGUI extends Application {
     @Override
     public void start(Stage primaryStage) {
-        primaryStage.setTitle("Hello World!");
-        Button btn = new Button();
-        btn.setText("Start");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
+        primaryStage.setTitle("PomTim");
+        primaryStage.getIcons().add(new Image("icons/logo_tomato_removebg.png"));
 
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        primaryStage.setScene(new Scene(root, 300, 250));
+        BorderPane root = new BorderPane();
+        Timer timerPane = new Timer();
+
+        root.setLeft(timerPane);
+        primaryStage.setScene(new Scene(root, 800, 400));
         primaryStage.getScene().getStylesheets().add("css/style.css");
         primaryStage.show();
     }
--- src/main/java/org/pomtim/gui/Timer.java
+++ src/main/java/org/pomtim/gui/Timer.java
@@ -1,4 +1,31 @@
 package org.pomtim.gui;
 
-public class Timer {
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+
+public class Timer  extends BorderPane {
+
+    private Label timerLabel;
+    private Button startButton, pauseButton, resetButton;
+
+    public Timer () {
+        timerLabel = new Label("25:00");
+        timerLabel.setStyle("-fx-font-size: 30px; -fx-font-weight: bold;");
+
+        startButton = new Button("Start");
+        pauseButton = new Button("Pause");
+        resetButton = new Button("Reset");
+
+        HBox controls = new HBox(10, startButton, pauseButton, resetButton);
+        controls.setAlignment(Pos.CENTER);
+
+        VBox timerBox = new VBox(10, timerLabel, controls);
+        timerBox.setAlignment(Pos.CENTER);
+
+        this.setCenter(timerBox);
+    }
 }
